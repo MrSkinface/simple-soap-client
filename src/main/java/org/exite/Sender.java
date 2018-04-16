8af552dda249020fc7f3affa167fd26c185d719d
Levitsky Michael <levitskym@edi.su>
2018-04-16 15:44:42 +0300

    ua bosh

--- src/main/java/org/exite/Sender.java
+++ src/main/java/org/exite/Sender.java
@@ -1,13 +1,11 @@
 package org.exite;
 
 import org.apache.log4j.Logger;
-import org.exite.SoapExAPI.SoapExAPIException;
 import org.exite.config.*;
 import org.exite.service.Controller;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.LinkedList;
